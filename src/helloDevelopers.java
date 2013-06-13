17a4030f38484192dae252c7ec58619cd8272bab
iiuDevelopers <tsmgeo@hotmail.com>
2013-06-13 16:27:20 +0300

    push by egit

--- src/helloDevelopers.java
+++ src/helloDevelopers.java
@@ -7,6 +7,8 @@ public class helloDevelopers {
 	public static void main(String[] args) {
 		System.out.println("print something.");
 		System.out.println("Hello Developers"); 
+		
+		System.out.println("egit plugin ");
 	}
 
 }
